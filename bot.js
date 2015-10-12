9b6f159ed651537e5975abdea8db40df7a86cdbc
Kris Lange <lange649@umn.edu>
2015-10-12 16:36:00 -0500

    added sean feature

--- bot.js
+++ bot.js
@@ -5,11 +5,19 @@ var BOT_ID = process.env.BOT_ID;
 function respond(){
 	var incoming = JSON.parse(this.req.chunks[0]);
 	var yesRegex = /\b(yes)\b/i;
+	var seanRegex = /\b(sean)\b/i;
 
-	if(incoming.text && yesRegex.test(incoming.text)){
-		this.res.writeHead(200);
-		yesMsg();
-		this.res.end();
+	if(incoming.text){ 
+		if(yesRegex.test(incoming.text)){
+			this.res.writeHead(200);
+			yesMsg();
+			this.res.end();
+		}
+		else if(seanRegex.test(incoming.text)){
+			this.res.writeHead(200);
+			seanMsg();
+			this.res.end();			
+		}
 	}
 	else{
 		console.log("NO MATCH:\t[\"" + incoming.text + "\"]");
@@ -24,6 +32,42 @@ function yesMsg(){
 	var options;
 
 	botRes = "Yaaaaaas";
+
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+
+	body = {
+		"bot_id": BOT_ID,
+		"text": botRes
+	};
+
+	console.log("Transmitting: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+
+	botReq = HTTPS.request(options, function(res) {
+		if(res.statusCode == 202){
+			console.log("Success: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+		}
+		else{
+			console.log("bad status code " + res.statusCode);
+		}
+	});
+
+	botReq.on('error', function(err){
+		console.log("Failed: [\"" + botRes + "\"] to [" + BOT_ID + "] " + JSON.stringify(err));
+	});
+
+	botReq.end(JSON.stringify(body));
+}
+
+function yesMsg(){
+	var botRes, botReq;
+	var body;
+	var options;
+
+	botRes = "Long live the King!";
 
 	options = {
 		hostname: 'api.groupme.com',
