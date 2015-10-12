014e9281ab8ae7514429c26191d0156d800e6bd9
Kris Lange <lange649@umn.edu>
2015-10-12 16:43:12 -0500

    added ripMsg

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ function respond(){
 	var incoming = JSON.parse(this.req.chunks[0]);
 	var yesRegex = /\b(yes)\b/i;
 	var seanRegex = /\b(sean)\b/i;
+	var ripRegex = /\b(rip)\b/i;
 
 	if(incoming.text && yesRegex.test(incoming.text)){
 		this.res.writeHead(200);
@@ -15,6 +16,11 @@ function respond(){
 	else if(incoming.text && seanRegex.test(incoming.text)){
 		this.res.writeHead(200);
 		seanMsg();
+		this.res.end();			
+	}
+	else if(incoming.text && ripRegex.test(incoming.text)){
+		this.res.writeHead(200);
+		ripMsg();
 		this.res.end();			
 	}
 	else{
@@ -66,6 +72,42 @@ function seanMsg(){
 	var options;
 
 	botRes = "Long live the King!";
+
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+
+	body = {
+		"bot_id": BOT_ID,
+		"text": botRes
+	};
+
+	console.log("Transmitting: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+
+	botReq = HTTPS.request(options, function(res) {
+		if(res.statusCode == 202){
+			console.log("Success: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+		}
+		else{
+			console.log("bad status code " + res.statusCode);
+		}
+	});
+
+	botReq.on('error', function(err){
+		console.log("Failed: [\"" + botRes + "\"] to [" + BOT_ID + "] " + JSON.stringify(err));
+	});
+
+	botReq.end(JSON.stringify(body));
+}
+
+function ripMsg(){
+	var botRes, botReq;
+	var body;
+	var options;
+
+	botRes = "Press F to pay respects";
 
 	options = {
 		hostname: 'api.groupme.com',
