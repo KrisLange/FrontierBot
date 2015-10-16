ce4cd10f3ab63be3ad971905383ab61ce16467e9
Kris Lange <lange649@umn.edu>
2015-10-15 20:38:31 -0500

    added and chill

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ function respond(){
 	var yesRegex = /\b(yes)\b/i;
 	var seanRegex = /\b(sean)\b/i;
 	var ripRegex = /\b(rip)\b/i;
+    var andChillRegex = /[^\s]*\s(and|&)\s(chi*ll*)/i;
 
 	if(incoming.text && yesRegex.test(incoming.text)){
 		this.res.writeHead(200);
@@ -23,6 +24,11 @@ function respond(){
 		ripMsg();
 		this.res.end();			
 	}
+    else if(incoming.text && andChillRegex.test(incoming.text)){
+        this.res.writeHead(200);
+        andChillMsg(incoming.text);
+        this.res.end();
+    }
 	else{
 		console.log("NO MATCH:\t[\"" + incoming.text + "\"]");
 		this.res.writeHead(200);
@@ -137,5 +143,42 @@ function ripMsg(){
 
 	botReq.end(JSON.stringify(body));
 }
+
+function andChillMsg(msg){
+    var botRes, botReq;
+    var body;
+    var options;
+    
+    botRes = andChillRegex.exec(msg) + "llllll!!!!";
+    
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+    };
+    
+    body = {
+        "bot_id": BOT_ID,
+        "text": botRes
+    };
+    
+    console.log("Transmitting: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+    
+    botReq = HTTPS.request(options, function(res) {
+                           if(res.statusCode == 202){
+                           console.log("Success: [\"" + botRes + "\"] to [" + BOT_ID + "]");
+                           }
+                           else{
+                           console.log("bad status code " + res.statusCode);
+                           }
+                           });
+    
+    botReq.on('error', function(err){
+              console.log("Failed: [\"" + botRes + "\"] to [" + BOT_ID + "] " + JSON.stringify(err));
+              });
+    
+    botReq.end(JSON.stringify(body));
+}
+
 
 exports.respond = respond;
